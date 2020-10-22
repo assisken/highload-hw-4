4ec5536d99d3bfc640a9aa02d280e5475430acec
Nikita Zhiga <assisken@gmail.com>
2020-10-22 15:45:28 +0300

    Linter fix.

--- app.py
+++ app.py
@@ -28,9 +28,7 @@ def hello_world():
 def ask_grpc_server(phone: str) -> int:
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = server_pb2_grpc.PhoneVerificationStub(channel)
-        resp = stub.VerifyPhone(
-            server_pb2.PhoneRequest(phoneNumber=phone)
-        )
+        resp = stub.VerifyPhone(server_pb2.PhoneRequest(phoneNumber=phone))
     return resp.message
 
 
